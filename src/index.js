50a2b349505a5d59591168f2f25e8cb80abc397c
Julio Chávarri <julio.chavarri@doktuz.com>
2021-08-13 00:26:40 -0500

    Update dependencies and include global state in the App

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import MessageList from './components/MessageList'
+import { App } from './App'
+import './css/styles.css'
 
-ReactDOM.render(<MessageList />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
